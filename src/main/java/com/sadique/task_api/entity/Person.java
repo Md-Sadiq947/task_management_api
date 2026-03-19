a5a6ec1e250f9f24add9f68c949d1691ad8148ab
Md-Sadiq947 <mdsadique822499@gmail.com>
2026-03-19 15:24:22 +0000

    Used the lombok to reduce the boiler plate code

--- src/main/java/com/sadique/task_api/entity/Person.java
+++ src/main/java/com/sadique/task_api/entity/Person.java
@@ -1,12 +1,14 @@
 package com.sadique.task_api.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.Date;
 
 @Entity
 @Table(name = "person")
+@Data
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,59 +21,4 @@ public class Person {
     private LocalDate date_of_birth;
     private String country_of_birth;
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public LocalDate getDate_of_birth() {
-        return date_of_birth;
-    }
-
-    public void setDate_of_birth(LocalDate date_of_birth) {
-        this.date_of_birth = date_of_birth;
-    }
-
-    public String getCountry_of_birth() {
-        return country_of_birth;
-    }
-
-    public void setCountry_of_birth(String country_of_birth) {
-        this.country_of_birth = country_of_birth;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
